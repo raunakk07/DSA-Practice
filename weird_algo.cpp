ee13fd31e616ca88d4c0ddee25cb19819fac8471
Harshit Srivastava <91839831+raunakk07@users.noreply.github.com>
2025-06-06 20:40:49 +0530

    Add files via upload

---
+++ weird_algo.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    ll n;
+    cin>>n;
+    cout<<n<<" ";
+    while(n>1){
+        
+        if(n%2==0)
+            n = n/2;
+        else
+            n = 3*n+1;
+        cout<<n<<" ";
+    }
+}
